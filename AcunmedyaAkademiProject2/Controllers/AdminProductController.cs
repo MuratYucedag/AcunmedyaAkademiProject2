445b1c8ee12112124700e58b725a579f9132bfed
MuratYucedag <yucedagmurat23@gmail.com>
2025-02-05 15:43:45 +0300

    ...

---
+++ AcunmedyaAkademiProject2/Controllers/AdminProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunmedyaAkademiProject2.Entities;
+using AcunmedyaAkademiProject2.Context;
+
+namespace AcunmedyaAkademiProject2.Controllers
+{
+    public class AdminProductController : Controller
+    {
+        SweetContext context = new SweetContext();
+        public ActionResult ProductList()
+        {
+            var values = context.Products.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+    }
+}
